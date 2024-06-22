336825350b40c4d728011b4f54286a6af004a73b
ohadi <mastore.ohadi@br-group.ir>
2024-06-23 00:34:17 +0330

    change main for random create list

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,24 +1,27 @@
+
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         List<Integer> keyPresses = new ArrayList<>();
-
-        System.out.println("Enter the key presses (enter -1 to end):");
-        while (true) {
-            int key = scanner.nextInt();
-            if (key == -1) {
-                break;
+        for (int k = 1; k < 5; k++) {
+            keyPresses = new ArrayList<>();
+            System.out.println("start program for " + k + "th run");
+            for (int i = 1; i <= 7; i++) {
+                keyPresses.add(i);
             }
-            keyPresses.add(key);
-        }
+            Collections.shuffle(keyPresses);
 
-        int activationCount = LampActivator.countLampActivations(keyPresses);
-        System.out.println("Number of lamp activations: " + activationCount);
+            System.out.println(keyPresses);
+            LampContext.resetInstance();
+            int activationCount = LampActivator.countLampActivations(keyPresses);
+            System.out.println("Number of lamp activations: " + activationCount);
+            System.out.println("*****************************************");
+        }
     }
 }
